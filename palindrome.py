50e57f38d7887a3dcd1aa3af708fccaa94e964ab
Zena Creps <ffgurl07@yahoo.com>
2023-02-26 14:46:49 -0800

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,7 @@
 def palindrome(word):
     # a word spelled the same way forward and backward
     word = word.lower()
+    # returns a slice of an entire iterable in reverse
     return word[::-1] == word
 
 print(palindrome('Mother'))
