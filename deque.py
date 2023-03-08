fa47d4d4897a475abd244d9b4afdd99e0defcf86
Zynkah <ffgurl07@yahoo.com>
2023-03-08 10:11:46 -0800

    docs updated

--- deque.py
+++ deque.py
@@ -3,15 +3,18 @@ from collections import deque
 def palchecker(aString):
     chardeque = deque()
 
+    # iterate the letters in the string
     for ch in aString:
         chardeque.appendleft(ch)
 
     stillEqual = True
 
     while len(chardeque) > 1 and stillEqual:
+        # removes first and last letter and compares if they are equal
         first = chardeque.pop()
         last = chardeque.popleft()
         if first != last:
+            # if they are different letters, return false
             stillEqual = False
         
     return stillEqual
