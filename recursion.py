767287afca9afbbd972add8d2c0423a1c9fcd9df
Zynkah <ffgurl07@yahoo.com>
2023-03-09 09:19:10 -0800

    docs updated

--- recursion.py
+++ recursion.py
@@ -1,4 +1,9 @@
 def palindrome(s):
+    ''' 
+    - if a string is 0 or one letter long
+    - if a string has the first and last letters the same, and the remaining letters are a palindrome
+    - if the slice [1: -1]
+    '''
     return len(s) < 2 or s[0] == s[-1] and palindrome(s[1:-1])
 
 print(palindrome('kayak'))
